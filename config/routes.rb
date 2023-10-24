c6946b8e745c650911f8c11790b82ab6a54b3536
Kseniya Zdorenko <kseniya_zdorenko@MacBook-Pro-Kseniya-2.local>
2023-10-24 04:22:48 +0300

    First commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+  resources :posts do
+    resources :comments
+  end
+  get 'welcome/index'
+  get 'welcome/about'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "welcome#index"
 end
