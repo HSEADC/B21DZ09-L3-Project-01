4d7509f47e245fc1ddf3a58cc1f88c5f8ab60b56
Kseniya Zdorenko <kseniya_zdorenko@MacBook-Pro-Kseniya-2.local>
2023-10-29 16:04:44 +0300

    commit message

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  resources :comments
+
   resources :posts do
     resources :comments
   end
+  
+
   get 'welcome/index'
   get 'welcome/about'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
